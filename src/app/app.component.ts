61a51d002543e1f1248932421e09b24a114caf89
speedy <speed.of.lightt@gmail.com>
2018-10-05 15:58:27 +0800

    feat: set title in environment file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Title } from '@angular/platform-browser';
+import { environment as env } from '../environments/environment';
+
 @Component({
   selector: 'ooo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'muscle';
+  constructor(private title: Title) {
+    title.setTitle(env.title || 'Untitled');
+  }
 }
